363e9bebb3af3d8ce371d387dd67690da000c1d8
Paolo96700 <paolo.falcoapp@gmail.com>
2023-06-08 14:12:53 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="get">
+
+
+        <div class="container">
+            <h1>Strong Password Generator</h1>
+            <h2>Genera una password sicura</h2>
+            <div>
+                <p>Lunghezza passowrd:</p>
+                <input type="text">
+            </div>
+            <div>
+                <p>Consenti ripetizioni di uno o più caratteri:</p>
+                <input type="radio" name="" id=""><span>Sì</span>
+                <input type="radio" name="" id=""><span>No</span>
+            </div>
+            <div>
+                <button>Invia</button>
+                <button>Annulla</button>
+            </div>
+
+
+
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
